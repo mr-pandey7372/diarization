1c3d8a0643e6f62184d9cf5b6a76d93182fce205
Abhimanyu <pandeyabhimanyu10@example.com>
2026-01-14 15:12:56 +0530

    code updated

--- diarization.py
+++ diarization.py
@@ -4,8 +4,8 @@ pipeline = Pipeline.from_pretrained(
     "pyannote/speaker-diarization"
 )
 
-diarization = pipeline("sample11.wav")
+diarization = pipeline("test.wav")
 
 for turn, _, speaker in diarization.itertracks(yield_label=True):
     print(f"{turn.start:.1f}s - {turn.end:.1f}s : {speaker}")
-diarization.write_rttm("sample11.rttm")
+diarization.write_rttm("test.rttm")
